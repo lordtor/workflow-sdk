b6c8176a825d9deae2ea70ba290d6e1fdbb8ea2c
Yuriy Rumyantsev <rumyantsevyn@yandex.ru>
2026-04-28 16:58:55 +0300

    fix: fix redeclared nats import in otel wrapper

--- otel/otel.go
+++ otel/otel.go
@@ -3,19 +3,20 @@ package otel
 
 import (
 	"context"
-	"github.com/nats-io/nats.go"
-	"github.com/lordtor/workflow-sdk/telemetry"
+
+	natslib "github.com/nats-io/nats.go"
 	"github.com/lordtor/workflow-sdk/nats"
+	"github.com/lordtor/workflow-sdk/telemetry"
 )
 
 func InitTracing(ctx context.Context, serviceName string) (func(), error) {
 	return telemetry.InitTracing(ctx, serviceName)
 }
 
-func InjectNatsTraceContext(ctx context.Context, msg *nats.Msg) {
+func InjectNatsTraceContext(ctx context.Context, msg *natslib.Msg) {
 	nats.InjectNatsTraceContext(ctx, msg)
 }
 
-func ExtractNatsTraceContext(ctx context.Context, msg *nats.Msg) context.Context {
+func ExtractNatsTraceContext(ctx context.Context, msg *natslib.Msg) context.Context {
 	return nats.ExtractNatsTraceContext(ctx, msg)
 }
